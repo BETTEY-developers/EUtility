adffcb8557e4cf5b0f725465483dd405c3b3796f
秋风Elipese <caihongxuan2020@outlook.com>
2023-08-14 11:25:57 +0800

    Add a Unit to EUtility.Console.Message.

---
+++ EUtility.Console.BottomMessage.UnitTest/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace EUtility.Console.Message.UnitTest;
+using SConsole = System.Console;
+
+// ***** Bottom message unit test *****
+internal class Program
+{
+    static void Use_Default_Two_Unit_Formatter_Outputer_Create_Message()
+    {
+        SConsole.WriteLine()
+        Message.MessageUnit messageUnit = new()
+        {
+            Title = "Testunit1",
+            Description = "Just Test"
+        };
+        Message.MessageUnit messageUnit2 = new()
+        {
+            Title = "Testunit2",
+            Description = "Just Test"
+        };
+
+        Message.MessageOutputer messageOutputer = new()
+        {
+            messageUnit,
+            messageUnit2
+        };
+
+        messageOutputer.Write();
+
+    }
+    static void Main(string[] args)
+    {
+        SConsole.WriteLine("***** Bottom message unit test *****");
+        
+    }
+}
