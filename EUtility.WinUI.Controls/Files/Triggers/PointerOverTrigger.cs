10162d16a681ead6f7780d1971911356cbb5981f
秋风Elipese <1175971310@qq.com>
2024-01-11 14:01:54 +0800

    upd: Add trigger and Change Styles

---
+++ EUtility.WinUI.Controls/Files/Triggers/PointerOverTrigger.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Input;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace EUtility.WinUI.Controls.Files.Triggers;
+
+public sealed class PointerOverTrigger : StateTriggerBase
+{
+    private UIElement _target;
+    public UIElement Target
+    {
+        get { return _target; }
+        set
+        {
+            _target = value;
+            _target.PointerEntered += target_PointerEntered;
+            _target.PointerExited += target_PointerExited;
+        }
+    }
+
+    private void target_PointerExited(object sender, PointerRoutedEventArgs e)
+    {
+        SetActive(false);
+    }
+
+    private void target_PointerEntered(object sender, PointerRoutedEventArgs e)
+    {
+        SetActive(true);
+    }
+}
---
+++ EUtility.WinUI.Controls/Files/Triggers/PointerPressedTrigger.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Input;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace EUtility.WinUI.Controls.Files.Triggers;
+
+public sealed class PointerPressedTrigger : StateTriggerBase
+{
+    private UIElement _target;
+    public UIElement Target
+    {
+        get { return _target; }
+        set
+        {
+            _target = value;
+            _target.PointerPressed += target_PointerPressed; ;
+            _target.PointerReleased += target_PointerReleased;
+        }
+    }
+
+    private void target_PointerReleased(object sender, PointerRoutedEventArgs e)
+    {
+        SetActive(false);
+    }
+
+    private void target_PointerPressed(object sender, PointerRoutedEventArgs e)
+    {
+        SetActive(true);
+    }
+}
